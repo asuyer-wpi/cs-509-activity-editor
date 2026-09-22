6d6a3a717da20a80a4f415eda3a94c0496f7adec
asuyer-wpi <asuyer@wpi.edu>
2026-09-21 20:52:59 -0400

    feat: created template for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,47 @@
-import styles from "./page.module.css";
+"use client"
+import styles from "./page.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
-export default function Home() {
+export default function Page() {
   return (
     <main>
       <header>
         <h1 className={styles.pageTitle}>ActivityEditor</h1>
       </header>
+
+      <section id="reporters">
+        <h2 className={styles.sectionTitle}>Reporters</h2>
+        <ReporterInput />
+      </section>
+
+      <section id="activities">
+        <h2 className={styles.sectionTitle}>Activities</h2>
+      </section>
     </main>
-  );
+  )
 }
+
+
+function ReporterInput() {
+  function handleAddReporter() {
+    // TODO
+  }
+
+  return (
+    <span className={`${styles.nameAndAdd} ${styles.reporterInput}`}>
+      <label className={styles.nameLabel} htmlFor="reporter-name">
+        Enter reporter name:</label>
+      <input className={styles.nameInput} id="reporter-name" type="text" />
+      <button
+        className={`${styles.iconButton} ${styles.addButton}`}
+        onClick={handleAddReporter} >
+        <FontAwesomeIcon icon={faPlus} />
+        Add
+      </button>
+    </span>
+
+  )
+}
+
+
