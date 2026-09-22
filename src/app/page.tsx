e8bd94104f6f29afde64a26c55bd628123c6f5fa
asuyer-wpi <asuyer@wpi.edu>
2026-09-21 22:48:55 -0400

    feat(page): implemented reporter input form and Add Reporter use case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,34 @@
 import styles from "./page.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from "react"
+
+import { ActivityEditor, Activity, Reporter } from "@/model"
 
 export default function Page() {
+  const [editor, setEditor] = useState(new ActivityEditor())
+
+  /**
+   * Controller: adds reporter to the reporter list
+   * @param name - name of the reporter
+   */
+  function addReporter(name: string) {
+    setEditor(editor.withNewReporter(new Reporter(name)))
+  }
+
+  // TODO: remove once 
+  useEffect(() => {
+    console.log("Activities:")
+    for (const activity of editor.activities) {
+      console.log(`${activity.name} - ${activity.description}`)
+    }
+
+    console.log("Reporters:")
+    for (const reporter of editor.reporters) {
+      console.log(`${reporter.name}`)
+    }
+  }, [editor])
+
   return (
     <main>
       <header>
@@ -12,7 +38,7 @@ export default function Page() {
 
       <section id="reporters">
         <h2 className={styles.sectionTitle}>Reporters</h2>
-        <ReporterInput />
+        <ReporterInput addReporter={addReporter} />
       </section>
 
       <section id="activities">
@@ -22,25 +48,40 @@ export default function Page() {
   )
 }
 
+/**
+ * Input form for reporter attributes
+ * @prop addReporter - callback function for adding a reporter
+ */
+function ReporterInput({ addReporter }: { addReporter: (name: string) => void }) {
+  const reporterNameId = "reporter-name"
 
-function ReporterInput() {
-  function handleAddReporter() {
-    // TODO
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    // Prevent page reload
+    event.preventDefault();
+
+    // Get name value from input field. Add the reporter if name is nonempty
+    const name = document.getElementById(reporterNameId) as HTMLInputElement
+    if (name.value !== "") {
+      addReporter(name.value)
+
+      // Clear field
+      name.value = ""
+    }
   }
 
   return (
-    <span className={`${styles.nameAndAdd} ${styles.reporterInput}`}>
-      <label className={styles.nameLabel} htmlFor="reporter-name">
-        Enter reporter name:</label>
-      <input className={styles.nameInput} id="reporter-name" type="text" />
+    <form className={`${styles.nameAndAdd} ${styles.reporterInput}`} onSubmit={handleSubmit}>
+      <label className={styles.nameLabel} htmlFor={reporterNameId}>
+        Enter reporter name:
+      </label>
+      <input className={styles.nameInput} id={reporterNameId} type="text" />
       <button
         className={`${styles.iconButton} ${styles.addButton}`}
-        onClick={handleAddReporter} >
+        type="submit" >
         <FontAwesomeIcon icon={faPlus} />
         Add
       </button>
-    </span>
-
+    </form>
   )
 }
 
